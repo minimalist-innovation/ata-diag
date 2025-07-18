9eaee49db706893ff4a19ba5768deee94b0f8ba2
AidenYav <yavaiden@gmail.com>
2025-07-18 14:26:18 -0700

    Included index 0 (defaults) to industry and saas_type IDs to be more inclusive in the permuation of test cases as suggested.

--- tests/integration_tests/test_report_page_integration.py
+++ tests/integration_tests/test_report_page_integration.py
@@ -4,9 +4,9 @@ from report_page_integration import *
 #This reduces test cases from 72 -> 8
 @pytest.mark.parametrize("gen_recommendations", [True]) #[True, False])
 @pytest.mark.parametrize("annual_revenue_million", [1.1, 2.1, 4.1, 7.1])
-@pytest.mark.parametrize("industry_id", [3, 4])#[0, 3, 4])
+@pytest.mark.parametrize("industry_id", [0, 3, 4])
 @pytest.mark.parametrize("orientation", ["Horizontal"])#["Horizontal", "Vertical"])
-@pytest.mark.parametrize("saas_type_id", [1]) #[0, 1, 2])
+@pytest.mark.parametrize("saas_type_id", [0, 1]) #[0, 1, 2])
 #Individual Tests if needed
 # @pytest.mark.parametrize("saas_type_id, orientation, industry_id, annual_revenue_million, gen_recommendations", [
 #     (0, "Horizontal", 0, 1.1, True)
